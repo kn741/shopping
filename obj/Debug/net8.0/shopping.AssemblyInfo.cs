7be8d88958211356205e913b81311479cf6cfc48
Chien Yang <kanzaki741@gmail.com>
2024-06-20 08:06:46 +0800

    20240620

--- obj/Debug/net8.0/shopping.AssemblyInfo.cs
+++ obj/Debug/net8.0/shopping.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("shopping")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f40ed4edb1641712fe2f498f3661fe004298f4f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62eafd7129350343b4fd6539d815d329b914ca55")]
 [assembly: System.Reflection.AssemblyProductAttribute("shopping")]
 [assembly: System.Reflection.AssemblyTitleAttribute("shopping")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
